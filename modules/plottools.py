d0a132db76ba92d17290520893c8257574bb330c
Alan Degenhart <alandegenhart@gmail.com>
2020-01-02 13:57:46 -0500

    Update plottools.py
    - 'create_subplot' now determines the format of the created plot
    - Top and right spines are turned off by default

--- modules/plottools.py
+++ modules/plottools.py
@@ -13,6 +13,15 @@ def create_subplot(
     import numpy as np
     import matplotlib.pyplot as plt
 
+    # Matplotlib housekeeping -- set the renderer and the font type. Setting the
+    # renderer to 'PDF' (non-interactive) allows figures to be generated when
+    # running scripts from the command line. Setting the font types ensures that
+    # any text that is generated can be edited.
+    import matplotlib as mpl
+    mpl.use('PDF')
+    mpl.rcParams['pdf.fonttype'] = 42
+    mpl.rcParams['ps.fonttype'] = 42
+
     # Define plot shape, create figure
     fig_w = x_marg[0] + ax_w*n_col + (n_col - 1)*ax_sp + x_marg[1]
     fig_h = y_marg[0] + ax_h*n_row + (n_row - 1)*ax_sp + y_marg[1]
@@ -22,7 +31,6 @@ def create_subplot(
 
     # Iterate over subplots and create axes
     ax_hndl = []  # Axes handles
-    plot_ctr = 0
     for row in range(n_row):
         for col in range(n_col):
             # Define axis position. Axes are ordered left-to-right, top-to-bottom,
@@ -30,5 +38,9 @@ def create_subplot(
             x_ax = x_marg[0]/fig_w + col*(ax_w + ax_sp)/fig_w
             y_ax = y_marg[0]/fig_h + (n_row - row - 1)*(ax_h + ax_sp)/fig_h
             ax_hndl.append(fh.add_axes([x_ax, y_ax, ax_w/fig_w, ax_h/fig_h]))
+
+            # Turn off spines (borders)
+            ax_hndl[-1].spines['top'].set_visible(False)
+            ax_hndl[-1].spines['right'].set_visible(False)
 
     return ax_hndl
